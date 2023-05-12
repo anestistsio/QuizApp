7193f03847f914764b532836cf99e11b4166606e
anestistsio <anestis.tsio@gmail.com>
2023-05-12 13:00:50 +0300

    .

--- app/src/main/java/com/uop/quizapp/MainGame.java
+++ app/src/main/java/com/uop/quizapp/MainGame.java
@@ -1,6 +1,7 @@
 package com.uop.quizapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -32,6 +33,7 @@ public class MainGame extends AppCompatActivity {
         //pass selected category from SelecteCategory.class 2 to MainGame.class
         String selectedCategory = getIntent().getExtras().getString("selectedCategory");
         selected_category_tv.setText(selectedCategory);
+
         //playing team ,score and team names are
         playing_team = getIntent().getExtras().getString("playing_team");
         t1n = getIntent().getExtras().getString("team1Name");
@@ -51,23 +53,27 @@ public class MainGame extends AppCompatActivity {
         question_tv.setText(questionsLists.get(index).getQuestion());
         answer_tv.setText(questionsLists.get(index).getAnswer());
 
-        //TODO this hashmap is not in use yet
-
     }
-    //questions_end(View view) method starts when either correct or incorrect button is clicked
+    //questions_end method starts when either correct or incorrect button is clicked
     public void question_end (View view) {
-       //checks which button is clicked (correct or incorrect)
+        //initialize correct and incorrect sounds
+        final MediaPlayer correct_sound = MediaPlayer.create(this,R.raw.correct_sound);
+        final MediaPlayer incorrect_sound = MediaPlayer.create(this,R.raw.incorrect_sound);
+
+        //checks which button is clicked (correct or incorrect)
         which_button = String.valueOf(view.getId());
         Intent intent = new Intent( MainGame.this,SelectCategory.class);
         if (which_button.equals(String.valueOf(correct_bt.getId()))){
-            //if correct button clicked then raise the score to the winning team
+            //if correct button clicked then raise the score to the winning team and play correct_sound
+            correct_sound.start();
             if (playing_team.equals(t1n)){
                 t1s++;
             }else {
                 t2s++;
             }
         }else{
-            //if the incorret button clicked then change playing team
+            //if the incorret button clicked then change playing team and play incorrect sound
+            incorrect_sound.start();
             Toast.makeText(this, "the phone now is changing hands because " + playing_team + " lost", Toast.LENGTH_LONG).show();
             //intent.putExtra("playing_team", playing_team);
             if (playing_team.equals(t1n)){
